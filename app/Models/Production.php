7e986935ddf13dce1be4e467038fa66fbe53005d
dickyagita <dickyagitacahya@gmail.com>
2021-12-06 11:00:13 +0700

    edit production model

--- app/Models/Production.php
+++ app/Models/Production.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Production extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'demand', 
+        'balance', 
+        'deficit', 
+        'production',
+    ];
 }
