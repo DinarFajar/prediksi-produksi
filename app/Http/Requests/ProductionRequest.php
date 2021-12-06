a6dc34d0ffdeb7be3a7b11fb2e7c2cfc121a3ef5
dickyagita <dickyagitacahya@gmail.com>
2021-12-06 10:52:39 +0700

    move n rename StoreProductionRequest.php

--- app/Http/Requests/ProductionRequest.php
+++ app/Http/Requests/ProductionRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Production;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreProductionRequest extends FormRequest
+class ProductionRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
