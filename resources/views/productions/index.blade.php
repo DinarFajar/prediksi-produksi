7d33d727aee70747c41dd11a85f766408e1402c9
dickyagita <dickyagitacahya@gmail.com>
2021-12-07 13:19:38 +0700

    create index view

---
+++ resources/views/productions/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('title', 'Produksi')
+
+@push('styles')
+  <x-datatables type="styles" />
+@endpush
+
+@section('content')
+  <div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Produksi</h1>
+    
+    <div class="card shadow mb-4">
+      <div class="card-header py-3 d-flex align-items-center">
+        <h6 class="m-0 font-weight-bold text-primary mr-auto">Data Produksi</h6>
+        <button class="btn btn-primary btn-sm" type="button" onclick="location.assign('{{ route('productions.create') }}')">Tambah</button>
+      </div>
+      <div class="card-body">
+
+        <x-alert-messages />
+
+        <div class="table-responsive py-1 pr-1">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Permintaan</th>
+                <th>Sisa</th>
+                <th>Kekurangan</th>
+                <th>Produksi</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($productions as $production)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $production->demand }}</td>
+                  <td>{{ $production->balance }}</td>
+                  <td>{{ $production->deficit }}</td>
+                  <td>{{ $production->production }}</td>
+                  <td><a href="{{ route('productions.show', ['production' => $production->id]) }}">lihat detail</a></td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('scripts')
+  <x-datatables type="scripts" />
+@endpush
