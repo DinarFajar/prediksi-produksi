5bc9e53ff75f3a6078746e9572681240ca300c71
dickyagita <dickyagitacahya@gmail.com>
2021-12-08 22:54:06 +0700

    edit edit view

---
+++ resources/views/productions/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Produksi')
+
+@section('content')
+  <div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Produksi</h1>
+
+    <div class="card shadow mb-4">
+      <div class="card-header card-header-cs py-0 d-flex align-items-center">
+        <div class="back-icon py-3" title="kembali" onclick="location.assign('{{ route('productions.show', ['production' => $production->id]) }}')">
+          <i class="fas fw fa-arrow-left"></i>
+        </div>
+        <h6 class="m-0 ml-1 font-weight-bold text-primary">Edit Produksi</h6>
+      </div>
+      <div class="card-body">
+
+        <x-validation-errors />
+
+        <form action="{{ route('productions.update', ['production' => $production->id]) }}" method="POST">
+          @method('PUT')
+          @csrf
+          <div class="form-group">
+            <label for="demand"><b>Permintaan</b></label>
+            <input id="demand" class="form-control" type="number" name="demand" value="{{ old('demand', $production->demand) }}" autofocus>
+          </div>
+          <div class="form-group">
+            <label for="balance"><b>Sisa</b></label>
+            <input id="balance" class="form-control" type="number" name="balance" value="{{ old('balance', ($production->balance !== 0 ? $production->balance : '')) }}">
+          </div>
+          <div class="form-group">
+            <label for="deficit"><b>Kekurangan</b></label>
+            <input id="deficit" class="form-control" type="number" name="deficit" value="{{ old('deficit', ($production->deficit !== 0 ? $production->deficit : '')) }}">
+          </div>
+          <button class="btn btn-primary" type="submit">Simpan</button>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('scripts')
+  <script type="text/javascript">
+    const balance = document.getElementById('balance');
+    const deficit = document.getElementById('deficit');
+
+    document.body.onload = function() {
+      if((balance.value.trim().length !== 0) && Number.isInteger(parseInt(balance.value))) {
+        deficit.disabled = true;
+      } else if((deficit.value.trim().length !== 0) && Number.isInteger(parseInt(deficit.value))) {
+        balance.disabled = true;
+      }
+    };
+
+    balance.addEventListener('keyup', function() {
+      const value = this.value;
+      if((value.trim().length !== 0) && Number.isInteger(parseInt(value))) {
+        deficit.disabled = true;
+      } else {
+        deficit.disabled = false;
+      }
+    });
+
+    deficit.addEventListener('keyup', function() {
+      const value = this.value;
+      if((value.trim().length !== 0) && Number.isInteger(parseInt(value))) {
+        balance.disabled = true;
+      } else {
+        balance.disabled = false;
+      }
+    });
+  </script>
+@endpush
