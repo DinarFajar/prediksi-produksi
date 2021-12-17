bbbd97ed15667d1776bd6727c45bacda156853c9
dickyagita <dickyagitacahya@gmail.com>
2021-12-17 10:53:54 +0700

    create gallery module

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use App\Http\Requests\GalleryRequest;
+
+class GalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\GalleryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GalleryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gallery  $gallery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gallery  $gallery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\GalleryRequest  $request
+     * @param  \App\Models\Gallery  $gallery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(GalleryRequest $request, Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gallery  $gallery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gallery $gallery)
+    {
+        //
+    }
+}
