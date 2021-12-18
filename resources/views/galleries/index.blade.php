8f731313d307415e5f9dbc71c9da70e190a1f765
dickyagita <dickyagitacahya@gmail.com>
2021-12-18 10:06:07 +0700

    edit index view

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Galeri')
 
+@push('styles')
+  <link rel="stylesheet" href="{{ asset('vendor/VenoBox-master/dist/venobox.min.css') }}" type="text/css" media="screen" />
+@endpush
+
 @section('content')
   <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800">Galeri</h1>
@@ -17,7 +21,9 @@
         <div class="row">
           @foreach($galleries as $gallery)
             <div class="col-sm-3 mb-4">
-              <img class="img-fluid" src="{{ $gallery->fullUrl() }}" loading="lazy">
+              <a class="my-image-links" data-gall="gallery01" data-maxwidth="600px" href="{{ $gallery->fullUrl() }}" title="{{ $gallery->filename }}">
+                <img class="img-fluid" src="{{ $gallery->fullUrl() }}" loading="lazy">
+              </a>
             </div>
           @endforeach
         </div>
@@ -25,3 +31,12 @@
     </div>
   </div>
 @endsection
+
+@push('scripts')
+  <script type="text/javascript" src="{{ asset('vendor/VenoBox-master/dist/venobox.min.js') }}"></script>
+  <script type="text/javascript">
+    new VenoBox({
+      selector: '.my-image-links'
+    });
+  </script>
+@endpush
