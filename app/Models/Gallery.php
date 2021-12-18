49eb0210920d00c1dac810a9ef402b2adcc8d851
dickyagita <dickyagitacahya@gmail.com>
2021-12-18 07:39:58 +0700

    edit gallery model

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['filename'];
+
+    public function fullUrl() 
+    {
+        return asset('storage/galleries/'.$this->filename);
+    }
 }
