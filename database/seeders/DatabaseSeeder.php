4418fc2c4d3918ed590c82f8d4757503c714f33a
dickyagita <dickyagitacahya@gmail.com>
2021-12-18 09:34:17 +0700

    edit gallery seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             TemplateSeeder::class,
             UserSeeder::class,
             CompanySeeder::class,
+            GallerySeeder::class,
         ]);
     }
 }
--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\CarbonImmutable;
 
 class GallerySeeder extends Seeder
 {
@@ -13,6 +15,49 @@ class GallerySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $now = CarbonImmutable::now();
+
+        DB::table('galleries')->insert([
+            [
+                'filename' => 'cutting.jpg',
+                'created_at' => $now->subMinutes(7),
+                'updated_at' => $now->subMinutes(7),
+            ],
+            [
+                'filename' => 'cutting1.jpg',
+                'created_at' => $now->subMinutes(6),
+                'updated_at' => $now->subMinutes(6),
+            ],
+            [
+                'filename' => 'editing.jpg',
+                'created_at' => $now->subMinutes(5),
+                'updated_at' => $now->subMinutes(5),
+            ],
+            [
+                'filename' => 'Printingpola.jpg',
+                'created_at' => $now->subMinutes(4),
+                'updated_at' => $now->subMinutes(4),
+            ],
+            [
+                'filename' => 'semple.jpg',
+                'created_at' => $now->subMinutes(3),
+                'updated_at' => $now->subMinutes(3),
+            ],
+            [
+                'filename' => 'sewing.jpg',
+                'created_at' => $now->subMinutes(2),
+                'updated_at' => $now->subMinutes(2),
+            ],
+            [
+                'filename' => 'zahwa.jpg',
+                'created_at' => $now->subMinutes(1),
+                'updated_at' => $now->subMinutes(1),
+            ],
+            [
+                'filename' => 'Shofiya.png',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ]);
     }
 }
