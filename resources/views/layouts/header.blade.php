3ebfcc5b91d6c4167afd0556853e452dce5f81df
dickyagita <dickyagitacahya@gmail.com>
2021-12-21 22:29:31 +0700

    edit header layout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,8 +11,7 @@
     <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
-        <img class="img-profile rounded-circle" src="{{ asset('img/undraw_profile.svg') }}" style="object-fit: cover;">
+        <span class="mr-2 d-none d-lg-inline text-gray-600"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></span>
       </a>
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
