80bebb0c06b0f966cb860f510904b27ec1f26d73
dickyagita <dickyagitacahya@gmail.com>
2022-01-05 22:57:39 +0700

    add tanggal field to fillable property

--- app/Models/Permintaan.php
+++ app/Models/Permintaan.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Permintaan extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'permintaan';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tanggal',
+        'permintaan',
+        'sisa',
+        'kekurangan',
+    ];
 }
