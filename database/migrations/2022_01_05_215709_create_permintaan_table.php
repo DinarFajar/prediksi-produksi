cd16bb28b657b0d36f06757d2417192860892098
dickyagita <dickyagitacahya@gmail.com>
2022-01-05 22:21:16 +0700

    add tanggal column

--- database/migrations/2022_01_05_215709_create_permintaan_table.php
+++ database/migrations/2022_01_05_215709_create_permintaan_table.php
@@ -15,6 +15,7 @@ class CreatePermintaanTable extends Migration
     {
         Schema::create('permintaan', function (Blueprint $table) {
             $table->id();
+            $table->datetime('tanggal');
             $table->integer('permintaan');
             $table->integer('sisa')->default(0);
             $table->integer('kekurangan')->default(0);
