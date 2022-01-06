0fe032101d18a641abeae11489c11704161e32bf
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 11:55:48 +0700

    edit PermintaanController.php

--- app/Http/Controllers/PermintaanController.php
+++ app/Http/Controllers/PermintaanController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Permintaan;
-use App\Http\Requests\StorePermintaanRequest;
-use App\Http\Requests\UpdatePermintaanRequest;
+use App\Models\{Permintaan, Prediksi, Produksi};
+use App\Http\Requests\PermintaanRequest;
 
 class PermintaanController extends Controller
 {
+    private $dir = 'permintaan.';
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +16,9 @@ class PermintaanController extends Controller
      */
     public function index()
     {
-        //
+        $data['permintaan'] = Permintaan::all();
+
+        return view($this->dir.'index', $data);
     }
 
     /**
@@ -25,18 +28,43 @@ class PermintaanController extends Controller
      */
     public function create()
     {
-        //
+        return view($this->dir.'create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StorePermintaanRequest  $request
+     * @param  \App\Http\Requests\PermintaanRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StorePermintaanRequest $request)
+    public function store(PermintaanRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $prediksiValue = null;
+
+        if($data['sisa_or_kekurangan'] === 'sisa') {
+            $data['sisa'] = $data['sisa_or_kekurangan_value'];
+            $prediksiValue = (new FuzzyMamdani($data['permintaan'], $data['sisa_or_kekurangan_value']))->prediksiFix;
+        }
+
+        elseif($data['sisa_or_kekurangan'] === 'kekurangan') {
+            $data['kekurangan'] = $data['sisa_or_kekurangan_value'];
+            $prediksiValue = (new FuzzyMamdani($data['permintaan'], 0, $data['sisa_or_kekurangan_value']))->prediksiFix;
+        }
+
+        $permintaan = Permintaan::create($data);
+
+        $prediksi = Prediksi::create([
+            'permintaan_id' => $permintaan->id,
+            'prediksi' => $prediksiValue,
+        ]);
+
+        Produksi::create(['prediksi_id' => $prediksi->id]);
+
+        return redirect()
+            ->route('permintaan.index')
+            ->with('success', 'Data Permintaan berhasil ditambahkan');
     }
 
     /**
@@ -58,19 +86,39 @@ class PermintaanController extends Controller
      */
     public function edit(Permintaan $permintaan)
     {
-        //
+        $data['permintaan'] = $permintaan;
+
+        return view($this->dir.'edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdatePermintaanRequest  $request
+     * @param  \App\Http\Requests\PermintaanRequest  $request
      * @param  \App\Models\Permintaan  $permintaan
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdatePermintaanRequest $request, Permintaan $permintaan)
+    public function update(PermintaanRequest $request, Permintaan $permintaan)
     {
-        //
+        $data = $request->validated();
+
+        if($data['sisa_or_kekurangan'] === 'sisa') {
+            $data['sisa'] = $data['sisa_or_kekurangan_value'];
+            $data['kekurangan'] = 0;
+            $data['prediksi'] = (new FuzzyMamdani($data['permintaan'], $data['sisa_or_kekurangan_value']))->prediksiFix;
+        }
+
+        elseif($data['sisa_or_kekurangan'] === 'kekurangan') {
+            $data['sisa'] = 0;
+            $data['kekurangan'] = $data['sisa_or_kekurangan_value'];
+            $data['prediksi'] = (new FuzzyMamdani($data['permintaan'], 0, $data['sisa_or_kekurangan_value']))->prediksiFix;
+        }
+
+        $permintaan->update($data);
+
+        return redirect()
+            ->route('permintaan.index')
+            ->with('success', 'Data Permintaan berhasil diperbarui');
     }
 
     /**
@@ -81,6 +129,14 @@ class PermintaanController extends Controller
      */
     public function destroy(Permintaan $permintaan)
     {
-        //
+        $permintaan->prediksi->produksi()->delete();
+
+        $permintaan->prediksi()->delete();
+
+        $permintaan->delete();
+
+        return redirect()
+            ->route('permintaan.index')
+            ->with('success', 'Data Permintaan berhasil dihapus');
     }
 }
