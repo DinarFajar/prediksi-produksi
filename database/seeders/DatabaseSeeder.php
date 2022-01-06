7ebb17d9be96444b025e7de1965c0450efa835eb
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 21:58:13 +0700

    remove TemplateSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
-            TemplateSeeder::class,
             UserSeeder::class,
             CompanySeeder::class,
             GallerySeeder::class,
