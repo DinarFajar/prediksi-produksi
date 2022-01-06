b01fcb8fdb3fe8724b1eb3a4b7d69f6e825e92e5
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 13:04:24 +0700

    init prediksi module

---
+++ app/Http/Controllers/PrediksiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prediksi;
+use App\Http\Requests\PrediksiRequest;
+
+class PrediksiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\PrediksiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PrediksiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Prediksi  $prediksi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Prediksi $prediksi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prediksi  $prediksi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Prediksi $prediksi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\PrediksiRequest  $request
+     * @param  \App\Models\Prediksi  $prediksi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PrediksiRequest $request, Prediksi $prediksi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Prediksi  $prediksi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prediksi $prediksi)
+    {
+        //
+    }
+}
