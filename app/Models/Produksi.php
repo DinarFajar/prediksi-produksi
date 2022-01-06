cf75c6bea4acea37c2d00a7952a8cf498f23ac38
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 14:09:19 +0700

    edit Produksi.php

--- app/Models/Produksi.php
+++ app/Models/Produksi.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Produksi extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'produksi';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'prediksi_id',
+        'produksi',
+    ];
+
+    public function prediksi()
+    {
+        return $this->belongsTo(Prediksi::class);
+    }
 }
