7d603e6b819128c98d91b1cd5ff0a8652a05aaa6
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 15:00:42 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,28 @@ use App\Http\Controllers;
 require __DIR__.'/auth.php';
 
 Route::middleware(['auth'])->group(function () {
+	// home
 	Route::get('/', Controllers\HomeController::class)->name('home');
-	Route::match(['get', 'post'], '/fuzzy-mamdani', Controllers\FuzzyMamdaniController::class)->name('fuzzy-mamdani');
-	Route::get('/galleries/all', [Controllers\GalleryController::class, 'all'])->name('galleries.all');
 	Route::get('/edit', [Controllers\HomeController::class, 'edit'])->name('home.edit');
-	Route::post('/predictions/{production}', [Controllers\PredictionController::class, 'store'])->name('predictions.store');
-	Route::post('/predictions/{production}/store-manually', [Controllers\PredictionController::class, 'storeManually'])->name('predictions.storeManually');
-	Route::get('/predictions/print', [Controllers\PredictionController::class, 'print'])->name('predictions.print');
-	Route::get('/productions/print', [Controllers\ProductionController::class, 'print'])->name('productions.print');
 	Route::put('/update', [Controllers\HomeController::class, 'update'])->name('home.update');
+	
+	// galeri
+	Route::get('/galeri/all', [Controllers\GalleryController::class, 'all'])->name('galeri.all');
+	
+	// prediksi
+	Route::get('/prediksi/cetak', [Controllers\PrediksiController::class, 'print'])->name('prediksi.print');
+	
+	// produksi
+	Route::get('/produksi/cetak', [Controllers\ProduksiController::class, 'print'])->name('produksi.print');
+	Route::post('/produksi/{produksi}', [Controllers\ProduksiController::class, 'store'])->name('produksi.store');
+	Route::post('/produksi/{produksi}/manual', [Controllers\ProduksiController::class, 'storeManually'])->name('produksi.storeManually');
+	
+	// others
+	Route::match(['get', 'post'], '/fuzzy-mamdani', Controllers\FuzzyMamdaniController::class)->name('fuzzy-mamdani');
 
 	// resources
-	Route::resource('galleries', Controllers\GalleryController::class)->except(['show', 'edit', 'update']);
-	Route::resource('predictions', Controllers\PredictionController::class)
-		->parameters(['predictions' => 'production'])
-		->except(['create', 'store']);
-	Route::resource('productions', Controllers\ProductionController::class);
-	Route::resource('templates', Controllers\TemplateController::class);
-});
-
-Route::get('/fuzzy/{permintaan}/{sisa}/{kekurangan}', function(int $permintaan, int $sisa, int $kekurangan) {
-	$fuzzy = new App\Libraries\FuzzyMamdani($permintaan, $sisa, $kekurangan);
-
-	dd($fuzzy->meta());
+	Route::resource('galeri', Controllers\GalleryController::class)->except(['show', 'edit', 'update']);
+	Route::resource('permintaan', Controllers\PermintaanController::class)->except(['show']);
+	Route::resource('prediksi', Controllers\PrediksiController::class)->only(['index']);
+	Route::resource('produksi', Controllers\ProduksiController::class)->except(['create', 'show']);
 });
