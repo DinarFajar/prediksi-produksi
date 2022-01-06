779524ac3764102566fc43d5d4437f2f16154510
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 13:31:51 +0700

    edit Prediksi.php

--- app/Models/Prediksi.php
+++ app/Models/Prediksi.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Prediksi extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'prediksi';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'permintaan_id',
+        'prediksi',
+    ];
+
+    public function produksi()
+    {
+        return $this->hasOne(Produksi::class);
+    }
 }
