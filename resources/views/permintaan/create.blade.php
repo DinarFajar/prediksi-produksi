9d2ee9231851a7da79ea6a7f659ec7ad1d05b045
dickyagita <dickyagitacahya@gmail.com>
2022-01-06 23:11:59 +0700

    edit all pages of permintaan

---
+++ resources/views/permintaan/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('title', 'Tambah Permintaan')
+
+@section('content')
+  <div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Permintaan</h1>
+
+    <div class="card shadow mb-4">
+      <div class="card-header card-header-cs py-0 d-flex align-items-center">
+        <div class="back-icon py-3" title="kembali" onclick="location.assign('{{ route('permintaan.index') }}')">
+          <i class="fas fw fa-arrow-left"></i>
+        </div>
+        <h6 class="m-0 ml-1 font-weight-bold text-primary">Tambah Baru</h6>
+      </div>
+      <div class="card-body">
+
+        <x-validation-errors />
+
+        <form action="{{ route('permintaan.store') }}" method="POST">
+          @csrf
+          <div class="form-group">
+            <label for="tanggal"><b>Tanggal</b></label>
+            <input id="tanggal" class="form-control" type="date" name="tanggal" value="{{ old('tanggal') }}" autofocus required>
+          </div>
+          <div class="form-group">
+            <label for="permintaan"><b>Permintaan</b></label>
+            <input id="permintaan" class="form-control" type="number" name="permintaan" value="{{ old('permintaan') }}" required>
+          </div>
+          <div class="form-group">
+            <div class="mb-2">
+              <div class="form-check form-check-inline">
+                <input id="sisa" class="form-check-input" type="radio" name="sisa_or_kekurangan" value="sisa" onchange="focusToValue()" onclick="focusToValue()" {{ old('sisa_or_kekurangan') === 'sisa' ? 'checked' : null }} required>
+                <label class="form-check-label" for="sisa"><b>Sisa</b></label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input id="kekurangan" class="form-check-input" type="radio" name="sisa_or_kekurangan" value="kekurangan" onchange="focusToValue()" onclick="focusToValue()" {{ old('sisa_or_kekurangan') === 'kekurangan' ? 'checked' : null }} required>
+                <label class="form-check-label" for="kekurangan"><b>Kekurangan</b></label>
+              </div>
+            </div>
+            <input id="sisa_or_kekurangan_value" class="form-control" type="number" name="sisa_or_kekurangan_value" value="{{ old('sisa_or_kekurangan_value') }}" required>
+          </div>
+          <button class="btn btn-primary" type="submit">Tambah</button>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('scripts')
+  <script type="text/javascript">
+    function focusToValue() {
+      document.getElementById('sisa_or_kekurangan_value').focus();
+    }
+  </script>
+@endpush
---
+++ resources/views/permintaan/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('title', 'Permintaan')
+
+@push('styles')
+  <x-datatables type="styles" />
+@endpush
+
+@section('content')
+  <div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Permintaan</h1>
+    
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <button class="btn btn-primary btn-sm" type="button" onclick="location.assign('{{ route('permintaan.create') }}')">Tambah Baru</button>
+      </div>
+      <div class="card-body">
+
+        <x-alert-messages />
+
+        <div class="table-responsive py-1 pr-1">
+          <table id="dataTable" class="table table-bordered" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Tanggal</th>
+                <th>Permintaan</th>
+                <th>Sisa</th>
+                <th>Kekurangan</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($permintaan as $p)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $p->tanggal }}</td>
+                  <td>{{ $p->permintaan }}</td>
+                  <td>{{ $p->sisa }}</td>
+                  <td>{{ $p->kekurangan }}</td>
+                  <td>
+                    <form action="{{ route('permintaan.destroy', ['permintaan' => $p->id]) }}" method="POST">
+                      @method('DELETE')
+                      @csrf
+                      <a href="{{ route('permintaan.edit', ['permintaan' => $p->id]) }}">Edit</a>
+                      <span class="mx-1 text-black-50">|</span>
+                      <a class="text-danger" href="{{ route('permintaan.destroy', ['permintaan' => $p->id]) }}" onclick="deleteData()">Hapus</a>
+                    </form>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('scripts')
+  <x-datatables type="scripts" />
+  <script type="text/javascript">
+    function deleteData() {
+      event.preventDefault();
+
+      if(confirm('Anda yakin ingin menghapus data ini?')) {
+        event.currentTarget.closest('form').submit();
+      } else {
+        return false;
+      }
+    }
+  </script>
+@endpush
